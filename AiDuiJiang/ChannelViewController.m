9ec651517ea51d6ae894adc6a738e998113d252d
chenjishi <chenjishi313@gmail.com>
2015-12-10 15:25:01 +0800

    changed bundle

--- AiDuiJiang/ChannelViewController.m
+++ AiDuiJiang/ChannelViewController.m
@@ -16,6 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithRed:242.f / 255 green:242.f / 255 blue:242.f / 255 alpha:1.0f];
+    
+    CGFloat width = self.view.frame.size.width;
+    CGFloat height = self.view.frame.size.height;
+    
+    NSLog(@"id %@", [NSBundle mainBundle].bundleIdentifier);
     
     UILabel *titleLabel = [[UILabel alloc] init];
     titleLabel.backgroundColor = [UIColor clearColor];
@@ -23,9 +29,50 @@
     titleLabel.text = @"我的频道";
     [titleLabel sizeToFit];
     self.navigationItem.titleView = titleLabel;
-
     
+    CALayer *border = [CALayer layer];
+    border.frame = CGRectMake(0, 199, self.view.frame.size.width, 1);
+    border.backgroundColor = [UIColor colorWithRed:230.f / 255 green:230.f / 255 blue:230.f / 255 alpha:1.0f].CGColor;
+    
+    UIView *view = [[UIView alloc] init];
+    view.frame = CGRectMake(0, 0, self.view.frame.size.width, 200);
+    [view.layer addSublayer:border];
+    view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:view];
+    
+    UIView *container = [[UIView alloc] init];
+    container.backgroundColor = [UIColor whiteColor];
+    CGFloat y = view.frame.origin.y + view.frame.size.height + 20;
+    container.frame = CGRectMake(0, y, self.view.frame.size.width, self.view.frame.size.height - y);
+    [self.view addSubview:container];
+    
+    UIView *nameView = [self viewWithTitle:@"频道名称" andName:@"未设置" atIndex:0];
+    [container addSubview:nameView];
+    
+    UIView *destView = [self viewWithTitle:@"目的地" andName:@"未设置" atIndex:1];
+    [container addSubview:destView];
 }
+
+- (UIView *)viewWithTitle:(NSString *)title andName:(NSString *)name atIndex:(NSUInteger) i {
+    CALayer *border = [CALayer layer];
+    CGFloat h = (i + 1) * 55.f;
+    border.frame = CGRectMake(0, h - 1, self.view.frame.size.width, 1);
+    border.backgroundColor = [UIColor colorWithRed:230.f / 255 green:230.f / 255 blue:230.f / 255 alpha:1.0f].CGColor;
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, i * 55, self.view.frame.size.width, 55)];
+    [view.layer addSublayer:border];
+    
+    UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 55.f)];
+    nameLabel.text = title;
+    nameLabel.textColor = [UIColor colorWithRed:75.f/255 green:75.f/255 blue:75.f/255 alpha:1.f];
+    nameLabel.font = [UIFont systemFontOfSize:17.f];
+    [nameLabel sizeToFit];
+    [view addSubview:nameLabel];
+    
+    return view;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
